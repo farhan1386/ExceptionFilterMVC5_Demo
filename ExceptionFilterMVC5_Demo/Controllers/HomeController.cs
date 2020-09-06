57eab84d1607826aeee3910a06904e2c4c203e31
Farhan Ahmed <farhan1086@gmail.com>
2020-09-06 21:36:51 +0530

    Add project files.

---
+++ ExceptionFilterMVC5_Demo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ExceptionFilterMVC5_Demo.Controllers
+{
+    [HandleError(ExceptionType = typeof(DivideByZeroException), View = "DivideByZero")]
+    [HandleError(ExceptionType = typeof(NullReferenceException), View = "NullReference")]
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new Exception("Something went wrong");
+        }
+
+        public ActionResult MethodOne()
+        {
+            throw new NullReferenceException();
+        }
+
+        public ActionResult MethodTwo()
+        {
+            throw new DivideByZeroException();
+        }
+    }
+}
